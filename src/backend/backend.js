fa51e8926cdfedc87143b422f43e9603ae7276b6
Dhruv Chawla <44582521+dc03@users.noreply.github.com>
2021-12-04 11:20:11 +0530

    Format code

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -52,38 +52,42 @@ app.listen(3000, function () {
 
 */
 
-
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const db="mongodb+srv://kuxi:kuxi@cluster0.5iuws.mongodb.net/weatherly?retryWrites=true&w=majority";
+const db = "mongodb+srv://kuxi:kuxi@cluster0.5iuws.mongodb.net/weatherly?retryWrites=true&w=majority";
 
-mongoose.connect(db,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true,
-}).then(()=>{
-    console.log("Connection Successful");
-}).catch((err)=>{
-    console.log("No connection");
-    console.log(err);
-})
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("[INFO] MongoDB connection successful");
+    })
+    .catch((err) => {
+        console.log("[ERROR] MongoDB connection failed");
+        console.log(err);
+    });
 
 const user = new mongoose.Schema({
-  uid: {
-    type: String,
-    unique: true,
-    index: true,
-    required: true,
-  },
-  pass: {
-    type: String,
-    required: true,
-  },
-  place:[{
-    type:String
-  }]
+    uid: {
+        type: String,
+        unique: true,
+        index: true,
+        required: true,
+    },
+    pass: {
+        type: String,
+        required: true,
+    },
+    place: [
+        {
+            type: String,
+        },
+    ],
 });
 
 const table = mongoose.model("users", user);
@@ -91,29 +95,34 @@ const table = mongoose.model("users", user);
 // New app using express module
 const app = express();
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 app.get("/", function (_req, res) {
-  res.sendFile(__dirname + "/login.html");
+    res.sendFile(__dirname + "/login.html");
 });
 
 app.post("/", function (req, res) {
-  console.log(req.body);
+    console.log(req.body);
 
-  var uid = String(req.body.uid);
-  var pass = String(req.body.pass);
+    var uid = String(req.body.uid);
+    var pass = String(req.body.pass);
 
-  var user1 = [new table({ uid: uid, pass: pass })];
-  table.insertMany(user1, (err, user) => {
-    if (err) console.log(err);
-    else console.log(user);
-  });
-  res.send("<h1>data stored</h1>");
+    table.insertMany([new table({ uid: uid, pass: pass })], (err, user) => {
+        if (err) {
+          console.log("[ERROR]");
+          console.log(err);
+        }
+        else {
+          console.log("[INFO] New user added");
+          console.log(user);
+        }
+    });
+    res.send("<h1>data stored</h1>");
 });
 
 app.listen(3000, function () {
-  console.log("server is running on port 3000");
+    console.log("[INFO] Server running on port 3000");
 });
